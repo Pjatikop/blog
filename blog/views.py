de8d43e2c3aa4192c01a86fb7cfd346c776fbb14
Pjatikop <Pjatikop@yandex.ru>
2023-07-26 19:15:51 +0300

    Blog

---
+++ blog/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import *
+from .forms import *
+from .utils import *
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class BlogHome(DataMixin, ListView):
+    model = Blog
+    template_name = "blog/index.html"
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Главная страница')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        return Blog.objects.filter(is_published=True).select_related('cat')
+
+
+class ShowPost(DataMixin, DetailView):
+    model = Blog
+    template_name = 'blog/post.html'
+    context_object_name = 'post'
+    slug_url_kwarg = 'post_slug'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class BlogCategory(DataMixin, ListView):
+    model = Blog
+    template_name = "blog/index.html"
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_queryset(self):
+        return Blog.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True).select_related('cat')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
+        c_def = self.get_user_context(title='Категория - ' + str(c.name), cat_selected=c.pk)
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+    form_class = AddPostForm
+    template_name = 'blog/addpage.html'
+    success_url = reverse_lazy('index')
+    login_url = reverse_lazy('login')
+    # raise_exception = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Добавление статьи')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'blog/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'blog/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
